fcd173189e28ac9d752c2c009be17be1d86a0270
pudelosha <pudel1985@gmail.com>
2025-01-28 22:26:08 +0100

    Initial commit: .NET Backend and Ionic Frontend

---
+++ Frontend/FrontendApp/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FrontendApp',
+  webDir: 'www'
+};
+
+export default config;
